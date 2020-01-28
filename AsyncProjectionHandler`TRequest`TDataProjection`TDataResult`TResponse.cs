8780c20358c35c3d6ed4f35acddd3deb5c48b5a1
arex388 <31530399+arex388@users.noreply.github.com>
2020-01-28 14:25:48 -0700

    Added one more extension.

---
+++ AsyncProjectionHandler`TRequest`TDataProjection`TDataResult`TResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+
+namespace Arex388.Extensions.MediatR {
+	public abstract class AsyncProjectionHandler<TRequest, TDataProjection, TDataResult, TResponse> :
+		AsyncProjectionHandler<TRequest, TDataProjection, TResponse>,
+		IAsyncProjectionHandler<TRequest, TDataProjection, TDataResult, TResponse>
+		where TRequest : IRequest<TResponse>
+		where TDataProjection : class
+		where TResponse : class {
+		public abstract TDataResult GetDataResult(
+			TRequest request);
+	}
+}
---
+++ IAsyncProjectionHandler`TRequest`TDataProjection`TDataResult`TResponse.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+namespace Arex388.Extensions.MediatR {
+	/// <summary>
+	/// An asynchronous returning projection request handler.
+	/// </summary>
+	/// <typeparam name="TRequest">The type of request being handled.</typeparam>
+	/// <typeparam name="TDataProjection">The type of data projection container.</typeparam>
+	/// <typeparam name="TDataResult">The type of data result container.</typeparam>
+	/// <typeparam name="TResponse">The type of response from the handler.</typeparam>
+	public interface IAsyncProjectionHandler<in TRequest, out TDataProjection, out TDataResult, TResponse> :
+		IAsyncProjectionHandler<TRequest, TDataProjection, TResponse>
+		where TRequest : IRequest<TResponse>
+		where TDataProjection : class {
+		TDataResult GetDataResult(
+			TRequest request);
+	}
+}
